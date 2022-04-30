67121da03b3defcefbd0373807461e757b38c053
Shahain Manujith <86903975+itsShahain@users.noreply.github.com>
2022-04-30 13:04:07 +0530

    Create 3_USACO_painting_the_barn.cpp

---
+++ Gold/3_USACO_painting_the_barn.cpp
@@ -0,0 +1,161 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+using namespace std::chrono;
+using ll = long long;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define nl '\n'
+// #define RUNTIME
+#define OJ_FileIO
+
+#ifdef OJ_FileIO
+void setIO(string s)
+{
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
+}
+#endif
+
+void solve()
+{
+  int actual[202][202]{0};
+  int actual_prefix[202][202]{0};
+  int farm[202][202]{0};
+  int prefix[202][202]{0};
+  int topDP[202]{0};
+  int bottomDP[202]{0};
+  int leftDP[202]{0};
+  int rightDP[202]{0};
+
+  int N, K;
+  cin >> N >> K;
+  while (N--)
+  {
+    int x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    x1++;
+    y1++;
+    farm[y1][x1]++;
+    farm[y1][x2 + 1]--;
+    farm[y2 + 1][x1]--;
+    farm[y2 + 1][x2 + 1]++;
+  }
+  int existing_coats = 0;
+  for (int x = 1; x <= 200; x++)
+  {
+    for (int y = 1; y <= 200; y++)
+    {
+      prefix[y][x] = farm[y][x] + prefix[y - 1][x] + prefix[y][x - 1] - prefix[y - 1][x - 1];
+      if (prefix[y][x] == K)
+        existing_coats++;
+    }
+  }
+  for (int x = 1; x <= 200; x++)
+  {
+    for (int y = 1; y <= 200; y++)
+    {
+      if (prefix[y][x] == K)
+      {
+        actual[y][x] = -1;
+      }
+      else if (prefix[y][x] == K - 1)
+      {
+        actual[y][x] = 1;
+      }
+    }
+  }
+  for (int x = 1; x <= 200; x++)
+  {
+    for (int y = 1; y <= 200; y++)
+    {
+      actual_prefix[y][x] = actual[y][x] + actual_prefix[y - 1][x] + actual_prefix[y][x - 1] - actual_prefix[y - 1][x - 1];
+    }
+  }
+
+  int x1, x2;
+  int cx1, cy1, cx2, cy2;
+
+  auto kadane = [&]()
+  {
+    int sm = 0;
+    int last_y = 1;
+    for (int y = 1; y <= 200; y++)
+    {
+      int row = actual_prefix[y][x2] - actual_prefix[y][x1 - 1] - actual_prefix[y - 1][x2] + actual_prefix[y - 1][x1 - 1];
+      sm = max(row, sm + row);
+      if (sm == row)
+      {
+        last_y = y;
+      }
+      cx1 = x1;
+      cx2 = x2;
+      cy1 = last_y;
+      cy2 = y;
+      rightDP[cx1] = max(sm, rightDP[cx1]);
+      leftDP[cx2 + 1] = max(sm, leftDP[cx2 + 1]);
+      topDP[cy1] = max(sm, topDP[cy1]);
+      bottomDP[cy2 + 1] = max(sm, bottomDP[cy2 + 1]);
+    }
+  };
+
+  for (x1 = 1; x1 <= 200; x1++)
+  {
+    for (x2 = x1; x2 <= 200; x2++)
+    {
+      kadane();
+    }
+  }
+
+  int delta_coats = 0;
+
+  for (int x = 0; x <= 201; x++)
+  {
+    int l = 0;
+    int r = 0;
+    for (int xl = x; xl >= 0; xl--)
+    {
+      l = max(l, leftDP[xl]);
+    }
+    for (int xr = x; xr <= 201; xr++)
+    {
+      r = max(r, rightDP[xr]);
+    }
+    delta_coats = max(delta_coats, l + r);
+  }
+  for (int y = 0; y <= 201; y++)
+  {
+    int d = 0;
+    int u = 0;
+    for (int yd = y; yd >= 0; yd--)
+    {
+      d = max(d, bottomDP[yd]);
+    }
+    for (int yu = y; yu <= 201; yu++)
+    {
+      u = max(u, topDP[yu]);
+    }
+    delta_coats = max(delta_coats, d + u);
+  }
+
+  cout << existing_coats + delta_coats;
+}
+
+int main()
+{
+  auto start = high_resolution_clock::now();
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+#ifdef OJ_FileIO
+  setIO("paintbarn");
+#endif
+  solve();
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+#ifdef RUNTIME
+  cout << "\n"
+       << "Run time : " << duration.count() << " microseconds";
+#endif
+  return 0;
+}
